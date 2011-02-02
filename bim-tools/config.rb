8e26708ee37740cd660f8f573f078334c5c47bef
jan <jan@brewsky.nl>
2011-02-02 21:42:57 +0000

    correction processor

---
+++ bim-tools/config.rb
@@ -0,0 +1,33 @@
+#       bimtools.rb
+#       
+#       Copyright 2011 Jan Brouwer <jan@brewsky.nl>
+#       
+#       This program is free software: you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation, either version 3 of the License, or
+#       (at your option) any later version.
+#       
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#       
+#       You should have received a copy of the GNU General Public License
+#       along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Configuration file for personal settings
+
+# IFC info
+author = "Architect"
+organization = "Building Designer Office"
+project_name = "Default Project"
+project_description = "Description of Default Project"
+ifc_person_id = "ID001"
+ifc_person_familyname = "Lastname"
+ifc_person_givenname = "Firstname"
+ifc_organisation_name = "Company"
+ifc_organisation_description = "Company description"
+ifc_site_name = "Default Site"
+ifc_site_description = "Description of Default Site"
+ifc_building_name = "Default Building"
+ifc_building_description = "Description of Default Building"
